a4c08228063813cfde0956a7f2c43d6af2e7fe1b
kaden-tech <kadenskinner@gmail.com>
2021-03-03 12:53:40 -0500

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,17 @@
+<!DOCTYPE>
+<html lang="en">
+     <head>
+          <title>Sign-Up</title>
+     </head>
+     <body>
+
+<div>
+     <form action="registration.php" method="POST">
+<div class="container">
+<h1>Sign-Up</h1>
+</div>          
+     </form>
+</div>
+
+     </body>
+</html>
