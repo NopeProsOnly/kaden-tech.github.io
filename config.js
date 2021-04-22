45e0ed06d69e2f887aba28ab520943d8fb4f1ffe
kaden-tech <kadenskinner@gmail.com>
2021-04-22 12:58:12 -0400

    notification update

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+    gcmAPIKey: 'BFpRo5RCgPsC3cNyWRyXda8ad-d8I879m5cG9aI4rMm2ZaaNRm7OHS0tdCs8UkqTAcvv-noeZxyW3vIww5VBJC0'
+};
