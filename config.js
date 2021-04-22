09450aa89478660a9c06c1f9733a10d27984c383
kaden-tech <kadenskinner@gmail.com>
2021-04-22 14:14:53 -0400

    update

--- config.js
+++
@@ -1,4 +0,0 @@
-window.GoogleSamples = window.GoogleSamples || {};
-window.GoogleSamples.Config = window.GoogleSamples.Config || {
-    gcmAPIKey: 'BFpRo5RCgPsC3cNyWRyXda8ad-d8I879m5cG9aI4rMm2ZaaNRm7OHS0tdCs8UkqTAcvv-noeZxyW3vIww5VBJC0'
-};
